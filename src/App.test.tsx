54764e9c5933d1be9ea99e0b878c360fd2f31a14
Maciej Kędra <maciejken@gmail.com>
2021-03-31 21:17:18 +0200

    use describe syntax in tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -11,83 +11,93 @@ let history: History;
 const mockStore = configureStore([]);
 let store: Store;
 
-beforeEach(() => {
-  history = createBrowserHistory();
+describe('App routes', () => {
+  beforeEach(() => {
+    history = createBrowserHistory();
+  });
+
+  describe('/', () => {
+    it('redirects to /sign-in if not authorized', () => {
+      history.push('/');
+      store = mockStore({ auth: { authorized: false } });
+      const { queryByTestId } = render(
+        <Provider store={store}>
+          <Router>
+            <App />
+          </Router>
+        </Provider>
+
+      );
+      expect(queryByTestId('image-gallery')).not.toBeInTheDocument();
+      expect(queryByTestId('signin-form')).toBeInTheDocument();
+      expect(queryByTestId('navbar')).not.toBeInTheDocument();
+    });
+
+    it('renders home page if authorized', () => {
+      history.push('/');
+      store = mockStore({ auth: { authorized: true } });
+      const { queryByTestId } = render(
+        <Provider store={store}>
+          <Router>
+            <App />
+          </Router>
+        </Provider>
+
+      );
+      expect(queryByTestId('signin-form')).not.toBeInTheDocument();
+      expect(queryByTestId('image-gallery')).toBeInTheDocument();
+      expect(queryByTestId('navbar')).toBeInTheDocument();
+      expect(queryByTestId('drawer')).not.toBeInTheDocument();
+    });
+  })
+
+  describe('/sign-in', () => {
+    it('renders signIn component if not authorized', () => {
+      history.push('/sign-in');
+      store = mockStore({ auth: { authorized: false } });
+      const { queryByTestId } = render(
+        <Provider store={store}>
+          <Router>
+            <App />
+          </Router>
+        </Provider>
+      );
+      expect(queryByTestId('signin-form')).toBeInTheDocument();
+      expect(queryByTestId('navbar')).not.toBeInTheDocument();
+      expect(queryByTestId('drawer')).not.toBeInTheDocument();
+    });
+
+    it('redirects to / if authorized', () => {
+      history.push('/sign-in');
+      store = mockStore({ auth: { authorized: true } });
+      const { queryByTestId } = render(
+        <Provider store={store}>
+          <Router>
+            <App />
+          </Router>
+        </Provider>
+      );
+      expect(queryByTestId('signin-form')).not.toBeInTheDocument();
+      expect(queryByTestId('navbar')).toBeInTheDocument();
+      expect(queryByTestId('drawer')).not.toBeInTheDocument();
+    });  
+  });
+
+  describe('/sign-up', () => {
+    it('renders signUp component', () => {
+      history.push('/sign-up');
+      store = mockStore({ auth: { authorized: false } });
+      const { queryByTestId } = render(
+        <Provider store={store}>
+          <Router>
+            <App />
+          </Router>
+        </Provider>
+      );
+      expect(queryByTestId('signup-form')).toBeInTheDocument();
+      expect(queryByTestId('navbar')).not.toBeInTheDocument();
+    });    
+  });
 });
 
-test('/ redirects to /sign-in if authorized === false', () => {
-  history.push('/');
-  store = mockStore({ auth: { authorized: false } });
-  const { queryByTestId } = render(
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
 
-  );
-  expect(queryByTestId('image-gallery')).not.toBeInTheDocument();
-  expect(queryByTestId('signin-form')).toBeInTheDocument();
-  expect(queryByTestId('navbar')).not.toBeInTheDocument();
-});
-
-test('/ renders home page if authorized === true', () => {
-  history.push('/');
-  store = mockStore({ auth: { authorized: true } });
-  const { queryByTestId } = render(
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
-
-  );
-  expect(queryByTestId('signin-form')).not.toBeInTheDocument();
-  expect(queryByTestId('image-gallery')).toBeInTheDocument();
-  expect(queryByTestId('navbar')).toBeInTheDocument();
-  expect(queryByTestId('drawer')).not.toBeInTheDocument();
-});
-
-test('/sign-in renders signIn if authorized === false', () => {
-  history.push('/sign-in');
-  store = mockStore({ auth: { authorized: false } });
-  const { queryByTestId } = render(
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
-  );
-  expect(queryByTestId('signin-form')).toBeInTheDocument();
-  expect(queryByTestId('navbar')).not.toBeInTheDocument();
-  expect(queryByTestId('drawer')).not.toBeInTheDocument();
-});
-
-test('/sign-in redirects to / if authorized === true', () => {
-  history.push('/sign-in');
-  store = mockStore({ auth: { authorized: true } });
-  const { queryByTestId } = render(
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
-  );
-  expect(queryByTestId('signin-form')).not.toBeInTheDocument();
-  expect(queryByTestId('navbar')).toBeInTheDocument();
-  expect(queryByTestId('drawer')).not.toBeInTheDocument();
-});
-
-test('/sign-up renders signUp', () => {
-  history.push('/sign-up');
-  store = mockStore({ auth: { authorized: false } });
-  const { queryByTestId } = render(
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
-  );
-  expect(queryByTestId('signup-form')).toBeInTheDocument();
-  expect(queryByTestId('navbar')).not.toBeInTheDocument();
-});
--- src/features/auth/__tests__/SignIn.test.tsx
+++ src/features/auth/__tests__/SignIn.test.tsx
@@ -5,95 +5,99 @@ import SignIn from '../SignIn';
 
 const authenticate = jest.fn();
 
-afterEach(() => {
-  authenticate.mockClear();
+describe('SignIn form', () => {
+  afterEach(() => {
+    authenticate.mockClear();
+  });
+  it('calls authenticate function on submit', () => {
+    const { queryByText } = render(
+      <MemoryRouter>
+        <SignIn authenticate={authenticate} />
+      </MemoryRouter>
+    );
+
+    const username = 'mockuser';
+    const password = 'mockpass23';
+    const basicAuth = { username, password };
+
+    fireEvent.input(screen.getByRole('textbox', { name: 'Nazwa użytkownika' }), {
+      target: { value: username }
+    });
+    fireEvent.input(screen.getByLabelText(/Hasło/), {
+      target: { value: password }
+    });
+    fireEvent.click(screen.getByRole('button', { name: 'Zaloguj' }));
+
+    expect(authenticate).toHaveBeenCalledWith(basicAuth);
+    expect(queryByText(/Wpisz co najmniej/)).not.toBeInTheDocument();
+  });
+
+  it('shows error if password is too short', () => {
+    const { queryByText } = render(
+      <MemoryRouter>
+        <SignIn authenticate={authenticate} />
+      </MemoryRouter>
+    );
+
+    const username = 'mockuser';
+    const password = 'm';
+
+    fireEvent.input(screen.getByRole('textbox', { name: 'Nazwa użytkownika' }), {
+      target: { value: username }
+    });
+    fireEvent.input(screen.getByLabelText(/Hasło/), {
+      target: { value: password }
+    });
+    fireEvent.click(screen.getByRole('button', { name: 'Zaloguj' }));
+
+    expect(authenticate).not.toHaveBeenCalled();
+    expect(queryByText(/Wpisz co najmniej/)).toBeInTheDocument();
+  });
+
+  it('shows error if username is too long', () => {
+    const { queryByText } = render(
+      <MemoryRouter>
+        <SignIn authenticate={authenticate} />
+      </MemoryRouter>
+    );
+
+    const username = 'veeeeeryLooooongUsername';
+    const password = 'mockpass23';
+
+    fireEvent.input(screen.getByRole('textbox', { name: 'Nazwa użytkownika' }), {
+      target: { value: username }
+    });
+    fireEvent.input(screen.getByLabelText(/Hasło/), {
+      target: { value: password }
+    });
+    fireEvent.click(screen.getByRole('button', { name: 'Zaloguj' }));
+
+    expect(authenticate).not.toHaveBeenCalled();
+    expect(queryByText(/Wpisz nie więcej/)).toBeInTheDocument();
+  });
+
+  it('shows error if username contains spaces', () => {
+    const { queryByText } = render(
+      <MemoryRouter>
+        <SignIn authenticate={authenticate} />
+      </MemoryRouter>
+    );
+
+    const username = 'mock user ';
+    const password = 'mockpass23';
+
+    fireEvent.input(screen.getByRole('textbox', { name: 'Nazwa użytkownika' }), {
+      target: { value: username }
+    });
+    fireEvent.input(screen.getByLabelText(/Hasło/), {
+      target: { value: password }
+    });
+    fireEvent.click(screen.getByRole('button', { name: 'Zaloguj' }));
+
+    expect(authenticate).not.toHaveBeenCalled();
+    expect(queryByText(/Nazwa użytkownika nie może zawierać spacji/)).toBeInTheDocument();
+  });
 });
 
-test('SignIn form submit button calls authenticate function', () => {
-  const { queryByText } = render(
-    <MemoryRouter>
-      <SignIn authenticate={authenticate} />
-    </MemoryRouter>
-  );
 
-  const username = 'mockuser';
-  const password = 'mockpass23';
-  const basicAuth = { username, password };
 
-  fireEvent.input(screen.getByRole('textbox', { name: 'Nazwa użytkownika' }), {
-    target: { value: username }
-  });
-  fireEvent.input(screen.getByLabelText(/Hasło/), {
-    target: { value: password }
-  });
-  fireEvent.click(screen.getByRole('button', { name: 'Zaloguj' }));
-
-  expect(authenticate).toHaveBeenCalledWith(basicAuth);
-  expect(queryByText(/Wpisz co najmniej/)).not.toBeInTheDocument();
-});
-
-test('SignIn form shows error if password is too short', () => {
-  const { queryByText } = render(
-    <MemoryRouter>
-      <SignIn authenticate={authenticate} />
-    </MemoryRouter>
-  );
-
-  const username = 'mockuser';
-  const password = 'm';
-
-  fireEvent.input(screen.getByRole('textbox', { name: 'Nazwa użytkownika' }), {
-    target: { value: username }
-  });
-  fireEvent.input(screen.getByLabelText(/Hasło/), {
-    target: { value: password }
-  });
-  fireEvent.click(screen.getByRole('button', { name: 'Zaloguj' }));
-
-  expect(authenticate).not.toHaveBeenCalled();
-  expect(queryByText(/Wpisz co najmniej/)).toBeInTheDocument();
-});
-
-test('SignIn form shows error if username is too long', () => {
-  const { queryByText } = render(
-    <MemoryRouter>
-      <SignIn authenticate={authenticate} />
-    </MemoryRouter>
-  );
-
-  const username = 'veeeeeryLooooongUsername';
-  const password = 'mockpass23';
-
-  fireEvent.input(screen.getByRole('textbox', { name: 'Nazwa użytkownika' }), {
-    target: { value: username }
-  });
-  fireEvent.input(screen.getByLabelText(/Hasło/), {
-    target: { value: password }
-  });
-  fireEvent.click(screen.getByRole('button', { name: 'Zaloguj' }));
-
-  expect(authenticate).not.toHaveBeenCalled();
-  expect(queryByText(/Wpisz nie więcej/)).toBeInTheDocument();
-});
-
-test('SignIn form shows error if username contains spaces', () => {
-  const { queryByText } = render(
-    <MemoryRouter>
-      <SignIn authenticate={authenticate} />
-    </MemoryRouter>
-  );
-
-  const username = 'mock user ';
-  const password = 'mockpass23';
-
-  fireEvent.input(screen.getByRole('textbox', { name: 'Nazwa użytkownika' }), {
-    target: { value: username }
-  });
-  fireEvent.input(screen.getByLabelText(/Hasło/), {
-    target: { value: password }
-  });
-  fireEvent.click(screen.getByRole('button', { name: 'Zaloguj' }));
-
-  expect(authenticate).not.toHaveBeenCalled();
-  expect(queryByText(/Nazwa użytkownika nie może zawierać spacji/)).toBeInTheDocument();
-});
